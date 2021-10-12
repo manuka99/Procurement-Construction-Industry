ad06c2e1801ffc164a0d308c01cfa1e8130eb8b8
yasas <manukayasas99@gmail.com>
2021-10-12 11:11:59 +0530

    build

--- WEB APP/Frontend/src/services/auth.service.js
+++
@@ -1,9 +0,0 @@
-import Api from "./api";
-
-export const Login = payload => {
-  return Api().post(`/public/login`, payload);
-};
-
-export const Register = payload => {
-  return Api().post(`/public/register`, payload);
-};
